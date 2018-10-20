fa3fafcebb2c98a006cf1360702ce07145b44f88
Aniruddha Shevle <aniruddhashevle@gmail.com>
2018-10-20 23:49:18 +0530

    styling and search list changes added

---
+++ src/components/SearchSelectParty/SearchParties/SearchedList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SearchedList = ({ partiesList, onSelectingResult }) =>
+    <div className="searched-list-wrapper">
+        {
+            partiesList.map(({
+                id,
+                name,
+                address,
+                aliasName
+            }) =>
+                <div key={id} id={id} className="search-list-row" onClick={onSelectingResult}>
+                    <div className="title">
+                        <span className="name">{name}</span>
+                        <span className="alias-name">({aliasName})</span>
+                    </div>
+                    <p className="address">{address}</p>
+                </div>
+            )
+        }
+    </div>
+
+export default SearchedList;
--- src/components/SearchSelectParty/SearchParties/index.jsx
+++ src/components/SearchSelectParty/SearchParties/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getPartiesList } from '../../../redux/actions/search-parties-action';
+import SearchedList from './SearchedList';
 import DEFAULT_AVATAR from '../../assets/images/default_avatar.svg';
 
 class SearchParties extends Component {
@@ -13,17 +14,25 @@ class SearchParties extends Component {
     }
 
     onInputSearch = (e) => {
-        this.props.getPartiesList();
-        this.setState({ partyName: e.target.value })
+        const { value } = e.target;
+        this.props.getPartiesList(value);
+        this.setState({ partyName: value })
+    }
+
+    onSelectingResult = () => {
+        console.log('s');
     }
 
     render() {
         const {
             partyName
-        } = this.state;
+        } = this.state,
+            {
+                searchedPartiesList
+            } = this.props;
         return (
             <div className="seach-parties-wrapper">
-                <img src={DEFAULT_AVATAR} className="avatar-image" alt="default avatar" />
+                <img src={DEFAULT_AVATAR} className="avatar-image select-avatar" alt="default avatar" />
                 <div className="input-wrapper">
                     <label htmlFor="praty-name" className={`${partyName ? 'party-name-label' : 'party-name-label-default'}`}>
                         Enter Party Name
@@ -35,10 +44,21 @@ class SearchParties extends Component {
                         onChange={this.onInputSearch}
                         value={partyName}
                     />
+                    <SearchedList partiesList={searchedPartiesList} onSelectingResult={this.onSelectingResult} />
                 </div>
             </div>
         )
     }
 }
 
-export default connect(null, { getPartiesList })(SearchParties);
+function mapStateToProps(reduxState) {
+    const {
+        partiesList: {
+            searchedPartiesList
+        }
+    } = reduxState;
+
+    return { searchedPartiesList };
+}
+
+export default connect(mapStateToProps, { getPartiesList })(SearchParties);
