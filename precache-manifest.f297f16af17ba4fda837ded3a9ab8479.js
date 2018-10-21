17258f7843a68574526d3b3c746a9b6b438ae0e4
Aniruddha Shevle <aniruddhashevle@gmail.com>
2018-10-21 22:09:17 +0530

    Updates

---
+++ precache-manifest.f297f16af17ba4fda837ded3a9ab8479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/search-list-task/static/media/roboto.regular.df7b648c.ttf"
+  },
+  {
+    "revision": "455c545b07abfd0c58db840f51b59ceb",
+    "url": "/search-list-task/static/media/default_avatar_highlighted.455c545b.svg"
+  },
+  {
+    "revision": "55e9c38ef6a0bb2c5634a09e49652eb6",
+    "url": "/search-list-task/static/media/default_avatar.55e9c38e.svg"
+  },
+  {
+    "revision": "6a6d83d69996ef556681",
+    "url": "/search-list-task/static/js/runtime~main.6a6d83d6.js"
+  },
+  {
+    "revision": "df15e4e479af9ca991e7",
+    "url": "/search-list-task/static/js/main.df15e4e4.chunk.js"
+  },
+  {
+    "revision": "61c661db0cb2f190525e",
+    "url": "/search-list-task/static/js/1.61c661db.chunk.js"
+  },
+  {
+    "revision": "df15e4e479af9ca991e7",
+    "url": "/search-list-task/static/css/main.2c1a0535.chunk.css"
+  },
+  {
+    "revision": "4a2c5d735ebf02360d08e2bcc80aefd3",
+    "url": "/search-list-task/index.html"
+  }
+];
